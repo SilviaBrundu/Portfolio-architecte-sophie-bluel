c084162be5d4625671d5e656f094e8ad0ab32fa8
silvia brundu <silviabrundu13@gmail.com>
2023-09-12 10:31:31 +0200

    feat: add filter gallery

--- assets/script.js
+++ assets/script.js
@@ -1,26 +1,37 @@
+
 // creation du lien avec l api via fetch
 // on recupere les données avec then et on utilise json
-fetch('http://localhost:5678/api/works')
-    .then(response => response.json()) 
-    .then(project => {
-        createGallery(project)
-    })
+callApiWorks()
 
 fetch('http://localhost:5678/api/categories')
     .then(response => response.json()) 
     .then(categories => {
         createButton(categories)
-        console.log(categories)
+        const buttons = document.querySelectorAll('.categories')
+        for (let i = 0; i < buttons.length; i++){
+            buttons[i].addEventListener('click', (event) => {
+            callApiWorks(event.target.textContent)
+            })
+        }
+        //console.log(categories)
     }) 
-  
+
+function callApiWorks (filter) {
+    fetch('http://localhost:5678/api/works')
+    .then(response => response.json()) 
+    .then(projects => {
+       //console.log(filter)
+        //console.log(projects)
+    const filteredProjects = filter && filter !== 'Tous' ? projects.filter(project => project.category.name === filter): projects
+    createGallery(filteredProjects)
+    })
+}
 
 // fonction qui va afficher les images de la galerie
 function createGallery(project){
-    const filters = []
+    //console.log(project)
+    document.querySelector('.gallery').innerHTML = ''
     for (let i = 0; i < project.length; i ++) {
-        if (!filters.includes(project[i].category.name)){ //creation des 3 category, le "!" inverse une condition
-            filters.push(project[i].category.name)
-        } 
         const gallery = document.querySelector('.gallery')
         const cards = document.createElement ('figure')
         gallery.appendChild(cards)
@@ -31,7 +42,6 @@ function createGallery(project){
         title.innerHTML = project[i].title
         cards.appendChild(title)
     }
-    console.log(filters)
 }
 
 //fonction qui crée les boutons
